65f82181a1a1b3fa2d0351665f1b3d99107e17e2
Tushar Choudhary <92353428+tushar-c23@users.noreply.github.com>
2023-06-03 23:22:54 +0530

    Merge script written

---
+++ Assets/Scripts/GameMerge.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMerge : MonoBehaviour
+{
+    public void RunnerGameStart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void AssDestroyerStart()
+    {
+        SceneManager.LoadScene("Scene_Gameplay");
+    }
+}
